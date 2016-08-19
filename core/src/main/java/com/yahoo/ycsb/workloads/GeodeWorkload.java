a0347abcf8edb242c5a3655b8a6a7f7ee25e445f
Frans Ojala <virtualiteetti@gmail.com>
2016-08-19 13:58:16 +0300

    going to print all the identifiers for querying

--- core/src/main/java/com/yahoo/ycsb/workloads/GeodeWorkload.java
+++ core/src/main/java/com/yahoo/ycsb/workloads/GeodeWorkload.java
@@ -361,6 +361,7 @@ public class GeodeWorkload extends Workload {
     int numOfRetries = 0;
     do {
       ueRegion.putIfAbsent(ue.getIMSI(), ue);
+      System.out.println(ue.getIMSI());
       status = Status.OK; // TODO: check if we can use the return value from the put above.
       if (status == Status.OK) {
         break;
