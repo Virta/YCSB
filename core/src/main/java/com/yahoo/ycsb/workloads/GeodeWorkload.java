db7389d4094ecdaee2c3aad3dc0d0d63322212d2
Frans Ojala <virtualiteetti@gmail.com>
2016-08-19 10:06:50 +0300

    Test 2: continuation

--- core/src/main/java/com/yahoo/ycsb/workloads/GeodeWorkload.java
+++ core/src/main/java/com/yahoo/ycsb/workloads/GeodeWorkload.java
@@ -359,7 +359,7 @@ public class GeodeWorkload extends Workload {
   @Override
   public boolean doInsert(DB db, Object threadstate) {
     UE ue = new UE();
-    System.out.println(ue);
+    System.out.println(ue.getIMSI());
     while (!ueIDs.put(ue.getIMSI(), false)) ue = new UE();
     Status status;
     int numOfRetries = 0;
