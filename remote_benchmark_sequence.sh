75c3dac8f02ac442fb0f3f5e8619da9ed3d35c4d
Frans Ojala <frojala@nc-3.cs.helsinki.fi>
2016-09-09 10:37:28 +0300

    put the ssh process into the background

--- remote_benchmark_sequence.sh
+++ remote_benchmark_sequence.sh
@@ -8,6 +8,6 @@ exp=$5
 
 while read s i m t e servers; do
 	for host in $servers; do
-		ssh $host "/home/frojala/YCSB/remote-seq.sh $start $increment $max_servers $threads $exp"
+		ssh $host "/home/frojala/YCSB/remote-seq.sh $start $increment $max_servers $threads $exp" &
 	done
 done <<< $@
