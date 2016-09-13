083cb85b93689ab852ee44374cdf8222d489ad11
Frans Ojala <frojala@nc-3.cs.helsinki.fi>
2016-09-13 12:05:39 +0300

    new script to start multiple servers

---
+++ start-servers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+start=$1
+max_servers=$2
+host=$(hostname | tr -d '-')
+
+for to_start in $(seq $start $max_servers); do
+        if [[ $to_start -eq 0 ]]; then continue; fi
+        server_port=$((40403+to_start))
+        /home/frojala/apache-geode-src-1.0.0-incubating.M2/geode-assembly/build/install/apache-geode/bin/gfsh\
+                -e "connect" -e "start server \
+                --name=serv-$host-$to_start \
+                --server-port=$server_port \
+                --classpath=/home/frojala/YCSB/core/target/archive-tmp/core-0.11.0-SNAPSHOT.jar"
+done
