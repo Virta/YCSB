5c53954493ad43e951035fe58f8df635dddcfa2d
Frans Ojala <frojala@nc-3.cs.helsinki.fi>
2016-09-16 14:28:37 +0300

    some more logging in the scripts, also now the server starting script checks for an open port. new UEID file

--- remote_benchmark_sequence.sh
+++ remote_benchmark_sequence.sh
@@ -7,14 +7,20 @@ threads=$4
 exp=$5
 
 while read s i m t e servers; do
-	num_servers=0
 	for i in $(seq $start $increment $max_servers); do
+		num_servers=0
+	   if [[ ! $max_servers -eq 1 ]]; then
 		for host in $servers; do
 			ssh $host mkdir /home/frojala/EXPERIMENTS
 			ssh $host mkdir /home/frojala/EXPERIMENTS/$exp
+			date
 			ssh $host "/home/frojala/YCSB/start-servers.sh $((i - $((increment-1)))) $i" 2>&1 | tee /home/frojala/EXPERIMENTS/$exp/server_start_logs
+			date
 			echo "Servers $((i - $((increment-1)))) to $i started on $host."
 		done
+		ssh nc-3 '/home/frojala/apache-geode-src-1.0.0-incubating.M2/geode-assembly/build/install/apache-geode/bin/gfsh -e "connect" -e "rebalance"'
+	   fi
+		date
 		for host in $servers; do
 			ssh $host "/home/frojala/YCSB/benchmark_geode.sh /home/frojala/EXPERIMENTS/ $exp $threads $i $increment" &
 			num_servers=$((num_servers+1))
@@ -27,7 +33,7 @@ while read s i m t e servers; do
 					completed_servers=$((completed_servers+1))
 				fi
 			done
-			echo "completed servers: $completed_servers / $num_servers"
+			echo "$(date) completed servers: $completed_servers / $num_servers"
 			sleep 30
 		done
 	done
--- start-servers.sh
+++ start-servers.sh
@@ -6,13 +6,19 @@ host=$(hostname | tr -d '-')
 
 for to_start in $(seq $start $max_servers); do
         if [[ $to_start -eq 0 ]]; then continue; fi
+
         server_port=$((40403+to_start))
+	open=$(nc -z localhost $server_port; echo $?)
+	while [[ $open -eq 0 ]]; do
+		server_port=$((server_port + 1))
+		open=$(nc -z localhost $server_port; echo $?)
+	done
+
         /home/frojala/apache-geode-src-1.0.0-incubating.M2/geode-assembly/build/install/apache-geode/bin/gfsh\
-                -e "connect" -e "start server \
-                --name=serv-$host-$to_start \
+       	        -e "connect" -e "start server \
+       		--name=serv-$host-$to_start \
                 --server-port=$server_port \
-                --classpath=/home/frojala/YCSB/core/target/archive-tmp/core-0.11.0-SNAPSHOT.jar"
+       	        --classpath=/home/frojala/YCSB/core/target/archive-tmp/core-0.11.0-SNAPSHOT.jar"
 done
 
-/home/frojala/apache-geode-src-1.0.0-incubating.M2/geode-assembly/build/install/apache-geode/bin/gfsh\
-                -e "connect" -e "rebalance"
+#/home/frojala/apache-geode-src-1.0.0-incubating.M2/geode-assembly/build/install/apache-geode/bin/gfsh -e "connect" -e "rebalance"
