ff031297b19f271f36ef7f150a306f382c90cfd2
Frans Ojala <frojala@nc-3.cs.helsinki.fi>
2016-09-18 13:45:05 +0300

    a little modification on the script. new UEID file

--- remote_benchmark_sequence.sh
+++ remote_benchmark_sequence.sh
@@ -5,6 +5,7 @@ increment=$2
 max_servers=$3
 threads=$4
 exp=$5
+server_list
 
 function run_benchmark {
    while read server servers; do
@@ -16,6 +17,7 @@ function run_benchmark {
                 done
                 completed_servers=0
                 while [[ ! $completed_servers -eq $num_servers ]]; do
+			sleep 30
                         completed_servers=0
                         for host in $servers; do
                                 if ssh $host test -e "/home/frojala/EXPERIMENTS/$exp/$exp"S"$server/$exp"S"$server"_T"$th_counter/complete" ; then
@@ -23,30 +25,30 @@ function run_benchmark {
                                 fi
                         done
                         echo -e "\n\n$(date) $exp"S"$server"T"$th_counter: completed servers: $completed_servers / $num_servers.\n\n"
-                        sleep 30
                 done
         done
    done <<< $@
 }
 
 while read s inc m t e servers; do
-echo "	for host in $servers; do
+	server_list=$servers
+	for host in $server_list; do
 		ssh $host mkdir /home/frojala/EXPERIMENTS
 		ssh $host mkdir /home/frojala/EXPERIMENTS/$exp
 	done
 
-	run_benchmark 1 $servers
-"
+	run_benchmark 1 $server_list
+
 	for server in $(seq $start $increment $max_servers); do
 		num_servers=0
-		for host in $servers; do
+		for host in $server_list; do
 			date
 			ssh $host "/home/frojala/YCSB/start-servers.sh $((server - $((increment-1)))) $server" 2>&1 | tee /home/frojala/EXPERIMENTS/$exp/server_start_logs
 			date
 			echo -e "\n\nServers $((server - $((increment-1)))) to $server started on $host.\n\n"
 		done
 		ssh nc-3 '/home/frojala/apache-geode-src-1.0.0-incubating.M2/geode-assembly/build/install/apache-geode/bin/gfsh -e "connect" -e "rebalance"'
-	   date
-	   run_benchmark $server $servers
+		date
+		run_benchmark $server $server_list
 	done
 done <<< $@
